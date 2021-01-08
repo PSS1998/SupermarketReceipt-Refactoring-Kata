969dd1c9cfc049ac2fa302b01a1e9ded36a1b04d
RaminFaryadHaris <faryad_raminharis@yahoo.com>
2021-01-08 19:46:18 +0330

    create haveOffer function

--- java/src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ java/src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -5,8 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ShoppingCart {
-
+class ShoppingCart {
     private final List<ProductQuantity> items = new ArrayList<>();
     private Map<Product, Double> productQuantities = new HashMap<>();
 
@@ -30,22 +29,26 @@ public class ShoppingCart {
     void handleOffers(Receipt receipt, Map<Product, Offer> offers, SupermarketCatalog catalog) {
         productQuantities.keySet().forEach(product -> {
             Offer offer = offers.get(product);
-            if (offer != null && offer.offerType.haveMinimumRequiredAmount(productQuantities.get(product))) {
-                receipt.addDiscount(new Discount(product, offer.offerType.getDescription(offer.argument), -calDiscountAmount(productQuantities.get(product), offer, catalog.getUnitPrice(product))));
+            if (haveOffer(product, offer)) {
+                receipt.addDiscount(new Discount(product, offer.offerType.getDescription(offer.argument), calDiscountAmount(productQuantities.get(product), offer, catalog.getUnitPrice(product))));
             }
         });
+    }
+
+    private boolean haveOffer(Product product, Offer offer) {
+        return offer != null && offer.offerType.haveMinimumRequiredAmount(productQuantities.get(product));
     }
 
     private double calDiscountAmount(double quantity, Offer offer, double unitPrice) {
         switch (offer.offerType) {
             case TenPercentDiscount:
-                return quantity * unitPrice * offer.offerType.getDiscountPercent() / 100.0;
+                return -(quantity * unitPrice * offer.offerType.getDiscountPercent() / 100.0);
             case ThreeForTwo:
                 double ThreeForTowTotal = (quantity / offer.offerType.getDiscountUnit() * 2 * unitPrice) + quantity % offer.offerType.getDiscountUnit() * unitPrice;
-                return quantity * unitPrice - ThreeForTowTotal;
+                return -(quantity * unitPrice - ThreeForTowTotal);
             default:
                 double defaultTotal = offer.argument * (quantity / offer.offerType.getDiscountUnit()) + quantity % offer.offerType.getDiscountUnit() * unitPrice;
-                return unitPrice * quantity - defaultTotal;
+                return -(unitPrice * quantity - defaultTotal);
         }
     }
 }
