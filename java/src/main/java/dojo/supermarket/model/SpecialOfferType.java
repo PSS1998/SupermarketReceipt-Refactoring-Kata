0894c6d0d415b9bc157bfae94a1e4a89707dc278
RaminFaryadHaris <faryad_raminharis@yahoo.com>
2021-01-08 18:22:23 +0330

    add discountUnit to SpecialOfferType

--- java/src/main/java/dojo/supermarket/model/SpecialOfferType.java
+++ java/src/main/java/dojo/supermarket/model/SpecialOfferType.java
@@ -2,4 +2,18 @@ package dojo.supermarket.model;
 
 public enum SpecialOfferType {
     ThreeForTwo, TenPercentDiscount, TwoForAmount, FiveForAmount;
+
+    public int getDiscountUnit() {
+        switch (this){
+            case ThreeForTwo:
+                return 3;
+            case TwoForAmount:
+                return 2;
+            case FiveForAmount:
+                return 5;
+            default:
+                return 1;
+        }
+    }
+
 }
